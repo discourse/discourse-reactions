c60cf4dd41adbda6c44c9b71d2dacfbec3ce80c7
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2020-06-23 10:09:41 +1000

    FIX: bug that includes + pluck(:id) was returning duplicated post ids

--- lib/discourse_reactions/topic_view_extension.rb
+++ lib/discourse_reactions/topic_view_extension.rb
@@ -3,6 +3,11 @@
 module DiscourseReactions::TopicViewExtension
   def filter_post_types(posts)
     return super(posts) unless SiteSetting.discourse_reactions_enabled
-    super(posts).includes(reactions: { reaction_users: :user })
+    super(posts).includes(reactions: { reaction_users: :user }).group('posts.id')
+  end
+
+  def highest_post_number
+    reutrn super unless SiteSetting.discourse_reactions_enabled
+    super.is_a?(Hash) ? super.values[0] : super
   end
 end
--- spec/serializers/topic_view_serializer_spec.rb
+++ spec/serializers/topic_view_serializer_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require_relative '../fabricators/reaction_fabricator.rb'
+require_relative '../fabricators/reaction_user_fabricator.rb'
 
 describe TopicViewSerializer do
   fab!(:user_1) { Fabricate(:user) }
@@ -12,5 +14,13 @@ describe TopicViewSerializer do
     SiteSetting.discourse_reactions_enabled_reactions = "laughing|heart|-open_mouth|-cry|-angry|thumbsup|-thumbsdown"
     json = TopicViewSerializer.new(topic_view, scope: Guardian.new, root: false).as_json
     expect(json[:valid_reactions]).to eq(%w(laughing heart open_mouth cry angry thumbsup thumbsdown).to_set)
+  end
+
+  it 'does not duplicate when pluck' do
+    reaction_1 = Fabricate(:reaction, post: post_1)
+    user_2 = Fabricate(:user)
+    Fabricate(:reaction_user, reaction: reaction_1, user: user_1)
+    Fabricate(:reaction_user, reaction: reaction_1, user: user_2)
+    expect(topic_view.posts.pluck(:id)).to eq([post_1.id])
   end
 end
