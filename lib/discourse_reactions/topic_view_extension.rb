afd056ffd6d18ba2d2a890d2253c3f07ca26e659
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2020-06-23 10:32:26 +1000

    Revert "FIX: bug that includes + pluck(:id) was returning duplicated post ids"
    This reverts commit c60cf4dd41adbda6c44c9b71d2dacfbec3ce80c7.

--- lib/discourse_reactions/topic_view_extension.rb
+++ lib/discourse_reactions/topic_view_extension.rb
@@ -3,11 +3,6 @@
 module DiscourseReactions::TopicViewExtension
   def filter_post_types(posts)
     return super(posts) unless SiteSetting.discourse_reactions_enabled
-    super(posts).includes(reactions: { reaction_users: :user }).group('posts.id')
-  end
-
-  def highest_post_number
-    reutrn super unless SiteSetting.discourse_reactions_enabled
-    super.is_a?(Hash) ? super.values[0] : super
+    super(posts).includes(reactions: { reaction_users: :user })
   end
 end
