discourse/discourse-reactions

commit 26b659cea9d3b91c58799ab988cc94b7e88d9140

FIX: When post.likeAction is null do not error for deleted posts (#65)
We should not show the reactions for deleted posts where
likeAction is null; this is the behaviour of Likes in core.

assets/javascripts/initializers/discourse-reactions.js.es6
@@ -13,7 +13,7 @@ function initializeDiscourseReactions(api) {
   api.decorateWidget("post-menu:before-extra-controls", dec => {
     const post = dec.getModel();
 
-    if (!post) {
+    if (!post || !post.likeAction) {
       return;
     }
 
@@ -33,7 +33,7 @@ function initializeDiscourseReactions(api) {
       dec.widget.siteSettings.discourse_reactions_reaction_for_like;
     const post = dec.getModel();
 
-    if (!post) {
+    if (!post || !post.likeAction) {
       return;
     }
 

assets/javascripts/discourse/widgets/discourse-reactions-reaction-button.js.es6
@@ -59,7 +59,7 @@ export default createWidget("discourse-reactions-reaction-button", {
   buildAttributes(attrs) {
     const likeAction = attrs.post.likeAction;
     if (!likeAction) {
-      return;
+      return {};
     }
 
     let title;

commit 85ec703f02a4854f5a8ceafaa4694e8aa245cb15

FIX: regression was hiding reactions on your own post
Regression was introduced in https://github.com/discourse/discourse-reactions/commit/26b659cea9d3b91c58799ab988cc94b7e88d9140

assets/javascripts/initializers/discourse-reactions.js.es6
@@ -13,8 +13,7 @@ function initializeDiscourseReactions(api) {
 
   api.decorateWidget("post-menu:before-extra-controls", dec => {
     const post = dec.getModel();
-
-    if (!post || !post.likeAction) {
+    if (!post || post.deleted_at) {
       return;
     }
 
@@ -34,7 +33,7 @@ function initializeDiscourseReactions(api) {
       dec.widget.siteSettings.discourse_reactions_reaction_for_like;
     const post = dec.getModel();
 
-    if (!post || !post.likeAction) {
+    if (!post || post.deleted_at) {
       return;
     }
 