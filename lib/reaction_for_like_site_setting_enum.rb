ab649da946374e92f9a362a13ef427f05889b537
Ahmed Gagan <ahmedgagan6@gmail.com>
2021-04-10 13:48:26 +0530

    DEV: ensures we use valid reactions

--- lib/reaction_for_like_site_setting_enum.rb
+++ lib/reaction_for_like_site_setting_enum.rb
@@ -3,19 +3,15 @@
 require_dependency 'enum_site_setting'
 
 class ReactionForLikeSiteSettingEnum < EnumSiteSetting
-  HEART ||= 'heart'
-
   def self.valid_value?(val)
     values.any? { |v| v[:value] == val }
   end
 
   def self.values
-    @values ||= begin
-      reactions = SiteSetting.discourse_reactions_enabled_reactions.split('|').map do |reaction|
+    @values = begin
+      reactions = DiscourseReactions::Reaction.valid_reactions.map do |reaction|
         { name: reaction, value: reaction }
       end
-
-      [{ name: HEART, value: HEART }].concat(reactions)
     end
   end
 end
