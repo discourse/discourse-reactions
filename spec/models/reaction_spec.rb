4b859d65d149a7dbc5602f99548f5e937f28c6b9
Ahmed Gagan <ahmedgagan6@gmail.com>
2021-04-15 17:58:13 +0530

    DEV: Allows '-1' types of reaction (#76)

--- spec/models/reaction_spec.rb
+++ spec/models/reaction_spec.rb
@@ -5,7 +5,7 @@ require_relative '../fabricators/reaction_fabricator.rb'
 
 describe DiscourseReactions::Reaction do
   it 'knows which reactions are valid' do
-    SiteSetting.discourse_reactions_enabled_reactions = "laughing|heart|-open_mouth|-cry|-angry|thumbsup|-thumbsdown"
+    SiteSetting.discourse_reactions_enabled_reactions = "laughing|heart|open_mouth|cry|angry|thumbsup|thumbsdown"
     expect(described_class.valid_reactions).to eq(%w(laughing heart open_mouth cry angry thumbsup thumbsdown).to_set)
   end
 end
