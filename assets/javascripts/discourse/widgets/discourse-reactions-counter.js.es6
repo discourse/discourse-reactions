discourse/discourse-reactions

commit 6ceff967e4b6455714565b1dd22afc476a51e12f

REFACTOR: various refactorings

assets/javascripts/discourse/widgets/discourse-reactions-counter.js.es6
@@ -1,3 +1,4 @@
+import { makeArray } from "discourse-common/lib/helpers";
 import { createPopper } from "@popperjs/core";
 import { h } from "virtual-dom";
 import { iconNode } from "discourse-common/lib/icon-library";
@@ -86,7 +87,7 @@ export default createWidget("discourse-reactions-counter", {
       this.state.postId = this.attrs.post.id;
     }
 
-    CustomReaction.findReactionUsers(this.attrs.post.id, {
+    return CustomReaction.findReactionUsers(this.attrs.post.id, {
       reactionValue,
     }).then((reactions) => {
       reactions.reaction_users.forEach((reactionUser) => {
@@ -134,11 +135,7 @@ export default createWidget("discourse-reactions-counter", {
     const mainReaction = this.siteSettings
       .discourse_reactions_reaction_for_like;
 
-    if (
-      attrs.post.reactions &&
-      attrs.post.reactions.length === 1 &&
-      attrs.post.reactions[0].id === mainReaction
-    ) {
+    if (makeArray(attrs.post.reactions) === [mainReaction]) {
       classes.push("only-like");
     }
 
@@ -153,14 +150,14 @@ export default createWidget("discourse-reactions-counter", {
     if (attrs.post.reaction_users_count) {
       const post = attrs.post;
       const count = post.reaction_users_count;
+      if (count <= 0) {
+        return;
+      }
+
       const mainReaction = this.siteSettings
         .discourse_reactions_reaction_for_like;
       const mainReactionIcon = this.siteSettings.discourse_reactions_like_icon;
       const items = [];
-
-      if (count <= 0) {
-        return;
-      }
 
       items.push(
         this.attach(
@@ -172,24 +169,14 @@ export default createWidget("discourse-reactions-counter", {
         )
       );
 
-      if (
-        !(
-          post.reactions.length === 1 &&
-          post.reactions[0].id === mainReaction
-        )
-      ) {
+      if (!(makeArray(post.reactions) === [mainReaction])) {
         attrs.state = this.state;
         items.push(this.attach("discourse-reactions-list", attrs));
       }
 
       items.push(h("span.reactions-counter", count.toString()));
 
-      if (
-        post.yours &&
-        post.reactions &&
-        post.reactions.length === 1 &&
-        post.reactions[0].id === mainReaction
-      ) {
+      if (post.yours && makeArray(post.reactions) === [mainReaction]) {
         items.push(
           h(
             "div.discourse-reactions-reaction-button.my-likes",
@@ -209,15 +196,7 @@ export default createWidget("discourse-reactions-counter", {
     this.cancelCollapse();
     this.state.statePanelExpanded = false;
     this.state.reactionsPickerExpanded = false;
-
-    const container = document.getElementById(this.buildId(this.attrs));
-    container &&
-      container
-        .querySelectorAll(
-          ".discourse-reactions-state-panel.is-expanded, .discourse-reactions-reactions-picker.is-expanded, .user-list.is-expanded"
-        )
-        .forEach((popper) => popper.classList.remove("is-expanded"));
-
+    this._resetPopper();
     this.scheduleRerender();
   },
 
@@ -266,28 +245,34 @@ export default createWidget("discourse-reactions-counter", {
         popperElement.classList.add("is-expanded");
 
         _popperStatePanel && _popperStatePanel.destroy();
-        _popperStatePanel = this._applyPopper(trigger, popperElement);
+        _popperStatePanel = createPopper(trigger, popperElement, {
+          placement: "top",
+          modifiers: [
+            {
+              name: "offset",
+              options: {
+                offset: [0, -5],
+              },
+            },
+            {
+              name: "preventOverflow",
+              options: {
+                padding: 5,
+              },
+            },
+          ],
+        });
       }
     });
   },
 
-  _applyPopper(button, picker) {
-    return createPopper(button, picker, {
-      placement: "top",
-      modifiers: [
-        {
-          name: "offset",
-          options: {
-            offset: [0, -5],
-          },
-        },
-        {
-          name: "preventOverflow",
-          options: {
-            padding: 5,
-          },
-        },
-      ],
-    });
+  _resetPopper() {
+    const container = document.getElementById(this.buildId(this.attrs));
+    container &&
+      container
+        .querySelectorAll(
+          ".discourse-reactions-state-panel.is-expanded, .discourse-reactions-reactions-picker.is-expanded, .user-list.is-expanded"
+        )
+        .forEach((popper) => popper.classList.remove("is-expanded"));
   },
 });

assets/javascripts/discourse/widgets/discourse-reactions-actions.js.es6
@@ -274,7 +274,7 @@ export default createWidget("discourse-reactions-actions", {
                   return CustomReaction.toggle(params.postId, params.reaction)
                     .then(resolve)
                     .catch((e) => {
-                      bootbox.alert(this.extractErrors(e));
+                      bootbox.alert(this._extractErrors(e));
                       this._rollbackState(post);
                     });
                 });
@@ -286,7 +286,7 @@ export default createWidget("discourse-reactions-actions", {
                 CustomReaction.toggle(params.postId, params.reaction)
                   .then(resolve)
                   .catch((e) => {
-                    bootbox.alert(this.extractErrors(e));
+                    bootbox.alert(this._extractErrors(e));
                     this._rollbackState(post);
                   });
               });
@@ -413,7 +413,6 @@ export default createWidget("discourse-reactions-actions", {
 
     this.collapsePanels();
 
-    let selector;
     const mainReactionName = this.siteSettings
       .discourse_reactions_reaction_for_like;
     const post = this.attrs.post;
@@ -445,12 +444,13 @@ export default createWidget("discourse-reactions-actions", {
       this.toggleReaction(attrs);
       return CustomReaction.toggle(this.attrs.post.id, attrs.reaction).catch(
         (e) => {
-          bootbox.alert(this.extractErrors(e));
+          bootbox.alert(this._extractErrors(e));
           this._rollbackState(post);
         }
       );
     }
 
+    let selector;
     if (
       post.reactions &&
       post.reactions.length === 1 &&
@@ -481,35 +481,12 @@ export default createWidget("discourse-reactions-actions", {
           CustomReaction.toggle(this.attrs.post.id, toggleReaction)
             .then(resolve)
             .catch((e) => {
-              bootbox.alert(this.extractErrors(e));
+              bootbox.alert(this._extractErrors(e));
               this._rollbackState(post);
             });
         });
       });
     });
-  },
-
-  extractErrors(e) {
-    const xhr = e.xhr || e.jqXHR;
-
-    if (!xhr || !xhr.status) {
-      return I18n.t("errors.desc.network");
-    }
-
-    if (
-      xhr.status === 429 &&
-      xhr.responseJSON &&
-      xhr.responseJSON.extras &&
-      xhr.responseJSON.extras.wait_seconds
-    ) {
-      return I18n.t("discourse_reactions.reaction.too_many_request", {
-        count: xhr.responseJSON.extras.wait_seconds,
-      });
-    } else if (xhr.status === 403) {
-      return I18n.t("discourse_reactions.reaction.forbidden");
-    } else {
-      return I18n.t("errors.desc.unknown");
-    }
   },
 
   cancelCollapse() {
@@ -553,16 +530,6 @@ export default createWidget("discourse-reactions-actions", {
     this.state.reactionsPickerExpanded = false;
     this._resetPopper();
     this.scheduleRerender();
-  },
-
-  _resetPopper() {
-    const container = document.getElementById(this.buildId(this.attrs));
-    container &&
-      container
-        .querySelectorAll(
-          ".discourse-reactions-state-panel.is-expanded, .discourse-reactions-picker.is-expanded"
-        )
-        .forEach((popper) => popper.classList.remove("is-expanded"));
   },
 
   html(attrs) {
@@ -654,5 +621,38 @@ export default createWidget("discourse-reactions-actions", {
     post.reactions = reactions;
     post.reaction_users_count = reaction_users_count;
     this.scheduleRerender();
+  },
+
+  _extractErrors(e) {
+    const xhr = e.xhr || e.jqXHR;
+
+    if (!xhr || !xhr.status) {
+      return I18n.t("errors.desc.network");
+    }
+
+    if (
+      xhr.status === 429 &&
+      xhr.responseJSON &&
+      xhr.responseJSON.extras &&
+      xhr.responseJSON.extras.wait_seconds
+    ) {
+      return I18n.t("discourse_reactions.reaction.too_many_request", {
+        count: xhr.responseJSON.extras.wait_seconds,
+      });
+    } else if (xhr.status === 403) {
+      return I18n.t("discourse_reactions.reaction.forbidden");
+    } else {
+      return I18n.t("errors.desc.unknown");
+    }
+  },
+
+  _resetPopper() {
+    const container = document.getElementById(this.buildId(this.attrs));
+    container &&
+      container
+        .querySelectorAll(
+          ".discourse-reactions-state-panel.is-expanded, .discourse-reactions-picker.is-expanded"
+        )
+        .forEach((popper) => popper.classList.remove("is-expanded"));
   },
 });

assets/javascripts/discourse/widgets/discourse-reactions-list-emoji.js.es6
@@ -10,7 +10,7 @@ const DISPLAY_MAX_USERS = 19;
 let _popperReactionUserPanel;
 
 export default createWidget("discourse-reactions-list-emoji", {
-  tagName: "div.reaction",
+  tagName: "div.discourse-reactions-list-emoji",
 
   buildId: (attrs) =>
     `discourse-reactions-list-emoji-${attrs.post.id}-${attrs.reaction.id}`,
@@ -32,9 +32,7 @@ export default createWidget("discourse-reactions-list-emoji", {
 
     const reaction = attrs.reaction;
     const users = attrs.users || [];
-    const displayUsers = [];
-
-    displayUsers.push(h("span.heading", attrs.reaction.id));
+    const displayUsers = [h("span.heading", attrs.reaction.id)];
 
     if (!users.length) {
       displayUsers.push(h("div.center", h("div.spinner.small")));
@@ -77,7 +75,7 @@ export default createWidget("discourse-reactions-list-emoji", {
     ];
 
     if (!window.matchMedia("(hover: none)").matches) {
-      elements.push(h(`div.user-list`, h("div.container", displayUsers)));
+      elements.push(h("div.user-list", h("div.container", displayUsers)));
     }
 
     return elements;
@@ -85,35 +83,30 @@ export default createWidget("discourse-reactions-list-emoji", {
 
   _setupPopper(selector) {
     schedule("afterRender", () => {
-      let popperElement;
       const elementId = CSS.escape(this.buildId(this.attrs));
       const trigger = document.querySelector(`#${elementId}`);
-      popperElement = document.querySelector(`#${elementId} ${selector}`);
+      const popperElement = document.querySelector(`#${elementId} ${selector}`);
 
       if (popperElement) {
         _popperReactionUserPanel && _popperReactionUserPanel.destroy();
-        _popperReactionUserPanel = this._applyPopper(trigger, popperElement);
+        _popperReactionUserPanel = createPopper(trigger, popperElement, {
+          placement: "bottom",
+          modifiers: [
+            {
+              name: "offset",
+              options: {
+                offset: [0, -5],
+              },
+            },
+            {
+              name: "preventOverflow",
+              options: {
+                padding: 5,
+              },
+            },
+          ],
+        });
       }
-    });
-  },
-
-  _applyPopper(button, picker) {
-    return createPopper(button, picker, {
-      placement: "bottom",
-      modifiers: [
-        {
-          name: "offset",
-          options: {
-            offset: [0, -5],
-          },
-        },
-        {
-          name: "preventOverflow",
-          options: {
-            padding: 5,
-          },
-        },
-      ],
     });
   },
 });

assets/javascripts/discourse/widgets/discourse-reactions-list.js.es6
@@ -5,8 +5,6 @@ export default createWidget("discourse-reactions-list", {
   tagName: "div.discourse-reactions-list",
 
   html(attrs) {
-    const reactions = attrs.post.reactions;
-
     if (attrs.post.reaction_users_count <= 0) {
       return;
     }
@@ -14,7 +12,7 @@ export default createWidget("discourse-reactions-list", {
     return [
       h(
         "div.reactions",
-        reactions.map((reaction) =>
+        attrs.post.reactions.map((reaction) =>
           this.attach("discourse-reactions-list-emoji", {
             reaction,
             users: attrs.state[reaction.id],

assets/javascripts/discourse/widgets/discourse-reactions-picker.js.es6
@@ -22,7 +22,6 @@ export default createWidget("discourse-reactions-picker", {
 
   html(attrs) {
     if (attrs.reactionsPickerExpanded) {
-      const post = attrs.post;
       const reactions = this.siteSettings.discourse_reactions_enabled_reactions
         .split("|")
         .filter(Boolean);
@@ -37,6 +36,8 @@ export default createWidget("discourse-reactions-picker", {
         );
       }
 
+      const currentUserReaction = attrs.post.current_user_reaction;
+
       return [
         h(
           "div.container",
@@ -48,19 +49,17 @@ export default createWidget("discourse-reactions-picker", {
               reaction ===
               this.siteSettings.discourse_reactions_reaction_for_like
             ) {
-              isUsed = post.current_user_used_main_reaction;
+              isUsed = attrs.post.current_user_used_main_reaction;
             } else {
               isUsed =
-                post.current_user_reaction &&
-                post.current_user_reaction.id === reaction;
+                currentUserReaction && currentUserReaction.id === reaction;
             }
 
-            if (post.current_user_reaction) {
+            if (currentUserReaction) {
               canUndo =
-                post.current_user_reaction.can_undo &&
-                post.likeAction.canToggle;
+                currentUserReaction.can_undo && attrs.post.likeAction.canToggle;
             } else {
-              canUndo = post.likeAction.canToggle;
+              canUndo = attrs.post.likeAction.canToggle;
             }
 
             let title;
@@ -75,7 +74,7 @@ export default createWidget("discourse-reactions-picker", {
             return this.attach("button", {
               action: "toggle",
               data: { reaction },
-              actionParam: { reaction, postId: post.id, canUndo },
+              actionParam: { reaction, postId: attrs.post.id, canUndo },
               className: `pickable-reaction ${reaction} ${
                 canUndo ? "can-undo" : ""
               } ${isUsed ? "is-used" : ""}`,

commit d1af1917515d35ba5cbd2ad989f3a0d57511dd0e

FIX: regression caused by 6ceff967e4

assets/javascripts/discourse/widgets/discourse-reactions-counter.js.es6
@@ -1,4 +1,3 @@
-import { makeArray } from "discourse-common/lib/helpers";
 import { createPopper } from "@popperjs/core";
 import { h } from "virtual-dom";
 import { iconNode } from "discourse-common/lib/icon-library";
@@ -182,7 +181,12 @@ export default createWidget("discourse-reactions-counter", {
 
       items.push(h("span.reactions-counter", count.toString()));
 
-      if (post.yours && makeArray(post.reactions) === [mainReaction]) {
+      if (
+        post.yours &&
+        post.reactions &&
+        post.reactions.length === 1 &&
+        post.reactions[0].id === mainReaction
+      ) {
         items.push(
           h(
             "div.discourse-reactions-reaction-button.my-likes",

assets/stylesheets/desktop/discourse-reactions.scss
@@ -7,12 +7,4 @@
   nav.post-controls .show-replies {
     position: relative;
   }
-
-  .discourse-reactions-actions {
-    &.my-post:not(.custom-reaction-used).has-reactions {
-      .discourse-reactions-counter {
-        padding-right: 15px;
-      }
-    }
-  }
 }