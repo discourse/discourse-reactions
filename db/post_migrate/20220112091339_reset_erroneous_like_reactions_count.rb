e57ecbea740c89189608145a630e3b724669b2e0
OsamaSayegh <asooomaasoooma90@gmail.com>
2022-01-12 14:19:00 +0300

    delete reaction_users record too

--- db/post_migrate/20220112091339_reset_erroneous_like_reactions_count.rb
+++ db/post_migrate/20220112091339_reset_erroneous_like_reactions_count.rb
@@ -15,13 +15,27 @@ class ResetErroneousLikeReactionsCount < ActiveRecord::Migration[6.1]
     # AR enum in the Reaction model
     emoji_reaction_type = 0
 
-    DB.exec(<<~SQL, like_reaction: like_reaction, emoji_reaction_type: emoji_reaction_type)
-      UPDATE discourse_reactions_reactions
-      SET reaction_users_count = NULL
+    inconsistent_reactions = DB.query(<<~SQL, like_reaction: like_reaction, emoji_reaction_type: emoji_reaction_type)
+      SELECT id
+      FROM discourse_reactions_reactions
       WHERE
         reaction_type = :emoji_reaction_type AND
         reaction_value = :like_reaction AND
         reaction_users_count IS NOT NULL
+    SQL
+
+    return if inconsistent_reactions.size == 0
+    ids = inconsistent_reactions.map(&:id)
+
+    DB.exec(<<~SQL, ids: ids)
+      DELETE FROM discourse_reactions_reaction_users
+      WHERE reaction_id IN (:ids)
+    SQL
+
+    DB.exec(<<~SQL, ids: ids)
+      UPDATE discourse_reactions_reactions
+      SET reaction_users_count = NULL
+      WHERE id IN (:ids)
     SQL
   end
 
