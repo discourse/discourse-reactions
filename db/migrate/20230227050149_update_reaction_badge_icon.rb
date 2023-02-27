1c60affbfe1eb77ea8c7bc5a2ceeaf650a95731d
Nat <natalie.tay@discourse.org>
2023-02-27 18:33:09 +0800

    Only update if unedited

--- db/migrate/20230227050149_update_reaction_badge_icon.rb
+++ db/migrate/20230227050149_update_reaction_badge_icon.rb
@@ -2,6 +2,6 @@
 
 class UpdateReactionBadgeIcon < ActiveRecord::Migration[7.0]
   def change
-    execute "UPDATE badges SET icon = 'smile' WHERE name = 'First Reaction'"
+    execute "UPDATE badges SET icon = 'smile' WHERE name = 'First Reaction' and icon = 'far-smile'"
   end
 end
