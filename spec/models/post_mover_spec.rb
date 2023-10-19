7be4a70911760e82aa83c4e581288e26e281bc1d
David Battersby <info@davidbattersby.com>
2023-10-19 18:29:56 +0800

    fix flaky test

--- spec/models/post_mover_spec.rb
+++ spec/models/post_mover_spec.rb
@@ -9,10 +9,11 @@ describe PostMover do
   fab!(:user) { Fabricate(:user) }
   fab!(:topic_1) { Fabricate(:topic, user: user) }
   fab!(:topic_2) { Fabricate(:topic, user: user) }
+  fab!(:topic_3) { Fabricate(:topic, user: user) }
   fab!(:post_1) { Fabricate(:post, topic: topic_1, user: user) }
   fab!(:post_2) { Fabricate(:post, topic: topic_1, user: user) }
-  fab!(:reaction_1) { Fabricate(:reaction, post: post_1, reaction_value: "clap") }
-  fab!(:reaction_2) { Fabricate(:reaction, post: post_2, reaction_value: "+1") }
+  fab!(:reaction_1) { Fabricate(:reaction, post: post_1) }
+  fab!(:reaction_2) { Fabricate(:reaction, post: post_2) }
   fab!(:user_reaction_1) do
     Fabricate(:reaction_user, user: admin, reaction: reaction_1, post: post_1)
   end
@@ -22,21 +23,37 @@ describe PostMover do
 
   before { SiteSetting.discourse_reactions_enabled = true }
 
-  it "moved post still has existing reactions" do
+  it "new post has topic's first post reactions (OP)" do
+    expect(post_1.reactions).to include(reaction_1)
+    expect(topic_2.posts.count).to eq(0)
+
     expect {
-      topic_1.move_posts(admin, [post_2.id], { destination_topic_id: topic_2.id })
+      topic_1.move_posts(admin, [post_1.id], { destination_topic_id: topic_2.id })
     }.to change { topic_2.posts.count }.by(1)
 
-    reaction_emojis = topic_2.posts.last.reactions.pluck(:reaction_value)
-    expect(reaction_emojis).to include("+1")
+    expect(topic_2.posts.count).to eq(1)
+
+    new_post = topic_2.reload.posts.first
+    reaction_emojis = new_post.reactions.pluck(:reaction_value)
+
+    expect(new_post.reactions.count).to eq(1)
+    expect(reaction_emojis).to include(reaction_1.reaction_value)
   end
 
-  it "new post has topic's first post reactions (OP)" do
-    expect {
-      topic_1.move_posts(admin, [topic_1.first_post.id], { destination_topic_id: topic_2.id })
-    }.to change { topic_2.posts.count }.by(1)
+  it "moved post still has existing reactions" do
+    expect(post_2.reactions).to include(reaction_2)
+    expect(topic_3.posts.count).to eq(0)
 
-    reaction_emojis = topic_2.posts.last.reactions.pluck(:reaction_value)
-    expect(reaction_emojis).to include("clap")
+    expect {
+      topic_1.move_posts(admin, [post_2.id], { destination_topic_id: topic_3.id })
+    }.to change { topic_3.posts.count }.by(1)
+
+    expect(topic_3.posts.count).to eq(1)
+
+    new_post = topic_3.reload.posts.first
+    reaction_emojis = new_post.reactions.pluck(:reaction_value)
+
+    expect(new_post.reactions.count).to eq(1)
+    expect(reaction_emojis).to include(reaction_2.reaction_value)
   end
 end
