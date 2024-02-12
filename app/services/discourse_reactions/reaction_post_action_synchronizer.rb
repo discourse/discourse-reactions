e6f3ca690e6e9d67bfe6350421424fe5f5c27cb7
Martin Brennan <martin@discourse.org>
2024-02-12 15:50:17 +1000

    DEV: Go down the non-destructive route for synchronizer
    * Add a flag to prevent sync / history changes by default, it's a hidden site setting `discourse_reactions_like_sync_enabled`
    * Instead of deleting `PostAction` records in the synchronizer, set them to trashed
    * Create and delete `UserAction` `LIKED` and `WAS_LIKED` records in the synchronizer
    * Restore trashed `PostAction` records when possible in the synchronizer
    * Leave out the initial data migration backfill for now

--- app/services/discourse_reactions/reaction_post_action_synchronizer.rb
+++ app/services/discourse_reactions/reaction_post_action_synchronizer.rb
@@ -3,12 +3,12 @@
 module DiscourseReactions
   class ReactionPostActionSynchronizer
     def self.sync!
+      return if !SiteSetting.discourse_reactions_like_sync_enabled
+
       excluded_from_like = SiteSetting.discourse_reactions_excluded_from_like.to_s.split("|")
 
-      # Find all ReactionUser records that do not have a
-      # corresponding PostAction record, for any reactions
-      # that are not in excluded_from_like, and create a
-      # PostAction record for each.
+      # Find all ReactionUser records that do not have a corresponding PostAction record,
+      # for any reactions that are not in excluded_from_like, and create a PostAction record for each.
       sql_query = <<~SQL
         INSERT INTO post_actions(
           post_id, user_id, post_action_type_id, created_at, updated_at
@@ -21,24 +21,108 @@ module DiscourseReactions
           ON post_actions.user_id = ru.user_id
           AND post_actions.post_id = ru.post_id
         WHERE post_actions.id IS NULL
-         AND discourse_reactions_reactions.reaction_value NOT IN (:excluded_from_like)
+        #{excluded_from_like.any? ? " AND discourse_reactions_reactions.reaction_value NOT IN (:excluded_from_like)" : ""}
+        RETURNING post_actions.id
       SQL
-      DB.exec(sql_query, like: PostActionType.types[:like], excluded_from_like: excluded_from_like)
 
-      # Find all PostAction records that have a ReactionUser
-      # record that uses a reaction in the excluded_from_like
-      # list, and delete them.
+      inserted_post_action_ids =
+        DB.query_single(
+          sql_query,
+          like: PostActionType.types[:like],
+          excluded_from_like: excluded_from_like,
+        )
+
+      # Find all trashed PostAction records matching ReactionUser records, which are not in excluded_from_like,
+      # and untrash them.
       sql_query = <<~SQL
-        DELETE FROM post_actions
-        USING discourse_reactions_reaction_users ru
+        UPDATE post_actions
+        SET deleted_at = NULL, deleted_by_id = NULL, updated_at = NOW()
+        FROM discourse_reactions_reaction_users ru
         INNER JOIN discourse_reactions_reactions
           ON discourse_reactions_reactions.id = ru.reaction_id
-        WHERE post_actions.user_id = ru.user_id
-          AND post_actions.post_id = ru.post_id
-          AND post_actions.post_action_type_id = :like
-          AND discourse_reactions_reactions.reaction_value IN (:excluded_from_like)
+        WHERE post_actions.deleted_at IS NOT NULL AND post_actions.user_id = ru.user_id
+          AND post_actions.post_id = ru.post_id AND post_actions.post_action_type_id = :like
+        #{excluded_from_like.any? ? " AND discourse_reactions_reactions.reaction_value NOT IN (:excluded_from_like)" : ""}
       SQL
-      DB.exec(sql_query, like: PostActionType.types[:like], excluded_from_like: excluded_from_like)
+      recovered_post_action_ids =
+        DB.query_single(
+          sql_query,
+          like: PostActionType.types[:like],
+          excluded_from_like: excluded_from_like,
+        )
+
+      # Create the corresponding UserAction records for the PostAction records. In
+      # the ReactionManager, this is done via PostActionCreator.
+      #
+      # The only difference between LIKE and WAS LIKED is the user;
+      #   * LIKE is the post action user because they are the one who liked the post
+      #   * WAS LIKED is done by the post user, because they are the like-ee
+      sql_query = <<~SQL
+          INSERT INTO user_actions (
+            action_type, user_id, acting_user_id, target_post_id, target_topic_id, created_at, updated_at
+          )
+          SELECT :ua_like,
+                 post_actions.user_id,
+                 post_actions.user_id,
+                 post_actions.post_id,
+                 posts.topic_id,
+                 post_actions.created_at,
+                 post_actions.created_at
+          FROM post_actions
+          INNER JOIN posts ON posts.id = post_actions.post_id
+          WHERE post_actions.id IN (:post_action_ids);
+
+          INSERT INTO user_actions (
+            action_type, user_id, acting_user_id, target_post_id, target_topic_id, created_at, updated_at
+          )
+          SELECT :ua_was_liked,
+                 posts.user_id,
+                 post_actions.user_id,
+                 post_actions.post_id,
+                 posts.topic_id,
+                 post_actions.created_at,
+                 post_actions.created_at
+          FROM post_actions
+          INNER JOIN posts ON posts.id = post_actions.post_id
+          WHERE post_actions.id IN (:post_action_ids);
+      SQL
+      DB.exec(
+        sql_query,
+        ua_like: UserAction::LIKE,
+        ua_was_liked: UserAction::WAS_LIKED,
+        post_action_ids: (recovered_post_action_ids + inserted_post_action_ids).uniq,
+      )
+
+      # Find all PostAction records that have a ReactionUser record that
+      # uses a reaction in the excluded_from_like list, and trash them.
+      if excluded_from_like.any?
+        sql_query = <<~SQL
+          WITH deleted_post_actions AS (
+            UPDATE post_actions
+            SET deleted_at = NOW()
+            FROM discourse_reactions_reaction_users ru
+            INNER JOIN discourse_reactions_reactions ON discourse_reactions_reactions.id = ru.reaction_id
+            WHERE post_actions.user_id = ru.user_id
+              AND post_actions.post_id = ru.post_id
+              AND post_actions.post_action_type_id = :like
+              AND discourse_reactions_reactions.reaction_value IN (:excluded_from_like)
+            RETURNING post_actions.post_id, post_actions.user_id
+          )
+
+          DELETE FROM user_actions
+          USING deleted_post_actions
+          WHERE user_actions.target_post_id = deleted_post_actions.post_id
+          AND user_actions.acting_user_id = deleted_post_actions.user_id
+          AND user_actions.action_type IN (:ua_like, :ua_was_liked)
+        SQL
+        DB.exec(
+          sql_query,
+          like: PostActionType.types[:like],
+          excluded_from_like: excluded_from_like,
+          ua_like: UserAction::LIKE,
+          ua_was_liked: UserAction::WAS_LIKED,
+        )
+      end
     end
   end
 end
