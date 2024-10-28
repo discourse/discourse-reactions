4c70d3acebce452213f3c323cf8682ab3de45678
Bianca Nenciu <nbianca@users.noreply.github.com>
2024-10-28 19:58:06 +0200

    DEV: Fix mismatched column types (#317)
    The primary key is usually a bigint column, but the foreign key columns
    are usually of integer type. This can lead to issues when joining these
    columns due to mismatched types and different value ranges.
    
    This was using a temporary plugin / test API to make tests pass, but it
    is safe to alter these tables because they usually have less than 1M
    rows.

--- spec/plugin_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.configure do |config|
-  config.before(:suite) do
-    if defined?(migrate_column_to_bigint)
-      migrate_column_to_bigint(DiscourseReactions::ReactionUser, :reaction_id)
-    end
-  end
-end
