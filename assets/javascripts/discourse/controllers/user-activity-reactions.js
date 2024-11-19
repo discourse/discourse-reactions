3bc1597e4543df9f928a3e1766b7a4a7a0915c5a
David Taylor <david@taylorhq.com>
2024-11-19 10:25:31 +0000

    DEV: Update eslint config (#321)

--- assets/javascripts/discourse/controllers/user-activity-reactions.js
+++ assets/javascripts/discourse/controllers/user-activity-reactions.js
@@ -1,7 +1,7 @@
 import { tracked } from "@glimmer/tracking";
 import Controller, { inject as controller } from "@ember/controller";
 import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
+import { service } from "@ember/service";
 import CustomReaction from "../models/discourse-reactions-custom-reaction";
 
 export default class UserActivityReactions extends Controller {
