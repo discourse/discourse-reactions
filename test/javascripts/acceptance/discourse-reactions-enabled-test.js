fac3464f47008e20c96bd08b1bfaf13131a38db0
Sérgio Saquetim <1108771+megothss@users.noreply.github.com>
2024-12-30 16:22:49 -0300

    DEV: Show login form when clicking on like with no user logged (#331)

--- test/javascripts/acceptance/discourse-reactions-enabled-test.js
+++ test/javascripts/acceptance/discourse-reactions-enabled-test.js
@@ -1,4 +1,4 @@
-import { visit } from "@ember/test-helpers";
+import { click, visit } from "@ember/test-helpers";
 import { test } from "qunit";
 import { acceptance, exists } from "discourse/tests/helpers/qunit-helpers";
 import { default as ReactionsTopics } from "../fixtures/reactions-topic-fixtures";
@@ -29,11 +29,34 @@ acceptance("Discourse Reactions - Enabled", function (needs) {
   });
 });
 
+acceptance("Discourse Reactions - Enabled | Anonymous user", function (needs) {
+  needs.settings({
+    glimmer_post_menu_mode: "enabled",
+    discourse_reactions_enabled: true,
+    discourse_reactions_enabled_reactions: "otter|open_mouth",
+    discourse_reactions_reaction_for_like: "heart",
+    discourse_reactions_like_icon: "heart",
+  });
+
+  needs.pretender((server, helper) => {
+    const topicPath = "/t/374.json";
+    server.get(topicPath, () => helper.response(ReactionsTopics[topicPath]));
+  });
+
+  test("It shows reactions controls", async (assert) => {
+    await visit("/t/topic_with_reactions_and_likes/374");
+    await click(".actions button.btn-toggle-reaction-like");
+
+    assert.dom(".d-modal.login-modal").exists("login modal was displayed");
+  });
+});
+
 // TODO (glimmer-post-menu) remove this test when the post menu is removed from core
 acceptance("Discourse Reactions - Widgets", function (needs) {
   needs.user();
 
   needs.settings({
+    glimmer_post_menu_mode: "disabled",
     discourse_reactions_enabled: true,
     discourse_reactions_enabled_reactions: "otter|open_mouth",
     discourse_reactions_reaction_for_like: "heart",
@@ -52,5 +75,28 @@ acceptance("Discourse Reactions - Widgets", function (needs) {
       exists(".discourse-reactions-actions"),
       "reaction controls are available"
     );
+  });
+});
+
+// TODO (glimmer-post-menu) remove this test when the post menu is removed from core
+acceptance("Discourse Reactions - Widgets | Anonymous user", function (needs) {
+  needs.settings({
+    glimmer_post_menu_mode: "disabled",
+    discourse_reactions_enabled: true,
+    discourse_reactions_enabled_reactions: "otter|open_mouth",
+    discourse_reactions_reaction_for_like: "heart",
+    discourse_reactions_like_icon: "heart",
+  });
+
+  needs.pretender((server, helper) => {
+    const topicPath = "/t/374.json";
+    server.get(topicPath, () => helper.response(ReactionsTopics[topicPath]));
+  });
+
+  test("It shows reactions controls", async (assert) => {
+    await visit("/t/topic_with_reactions_and_likes/374");
+    await click(".actions button.btn-toggle-reaction-like");
+
+    assert.dom(".d-modal.login-modal").exists("login modal was displayed");
   });
 });
