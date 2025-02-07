0a4c5f3049a83d2f958a731ae1312aadb182af30
Martin Brennan <martin@discourse.org>
2025-02-07 10:26:01 +1000

    DEV: Review fixes

--- lib/discourse_reactions/posts_reaction_loader.rb
+++ lib/discourse_reactions/posts_reaction_loader.rb
@@ -6,16 +6,15 @@ module DiscourseReactions::PostsReactionLoader
       posts = object.posts.includes(:post_actions, reactions: { reaction_users: :user })
       post_ids = posts.map(&:id).uniq
       posts_reaction_users_count = TopicViewSerializer.posts_reaction_users_count(post_ids)
-      posts.each { |post| post.reaction_users_count = posts_reaction_users_count[post.id].to_i }
-
       post_actions_with_reaction_users =
         DiscourseReactions::TopicViewSerializerExtension.load_post_action_reaction_users_for_posts(
           post_ids,
         )
-
       posts.each do |post|
+        post.reaction_users_count = posts_reaction_users_count[post.id].to_i
         post.post_actions_with_reaction_users = post_actions_with_reaction_users[post.id] || {}
       end
+
       object.instance_variable_set(:@posts, posts)
     end
   end
