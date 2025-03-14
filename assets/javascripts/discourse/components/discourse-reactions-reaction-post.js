5e59d15f4d4ad653c7c6202ba6dc9c02378311c7
Juan David Martínez Cubillos <jd.martinez1062@gmail.com>
2025-03-14 15:33:05 -0500

    UX: Reactions user activity page shows names on mentions when  site setting is true (#355)
    * UX: Reactions user activity page shows names on mentions when  site setting is true
    
    * linting issues
    
    * added support for unicode usernames
    
    * fix other usernames not changed to the name correctly
    
    * added unicode usernames test

--- assets/javascripts/discourse/components/discourse-reactions-reaction-post.js
+++ assets/javascripts/discourse/components/discourse-reactions-reaction-post.js
@@ -1,14 +1,112 @@
 import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
 import { equal } from "@ember/object/computed";
 import { service } from "@ember/service";
+import { ajax } from "discourse/lib/ajax";
 import getURL from "discourse/lib/get-url";
 import { emojiUrlFor } from "discourse/lib/text";
 
+let cachedNames = new Map();
+let pendingSearch = null;
+
 export default class DiscourseReactionsReactionPost extends Component {
   @service site;
+  @service siteSettings;
+
+  @tracked updatedExcerpt = this.args.reaction.post.excerpt;
+  @tracked updatedExpandedExcerpt = this.args.reaction.post.expandedExcerpt;
 
   @equal("args.reaction.post.post_type", "site.post_types.moderator_action")
   moderatorAction;
+
+  constructor() {
+    super(...arguments);
+    this.updateMentionedUsernames();
+  }
+
+  @action
+  async updateMentionedUsernames() {
+    this.updatedExcerpt = await this.replaceMentionsWithFullNames(
+      this.args.reaction.post.excerpt
+    );
+    this.updatedExpandedExcerpt = await this.replaceMentionsWithFullNames(
+      this.args.reaction.post.expandedExcerpt
+    );
+  }
+
+  async replaceMentionsWithFullNames(text) {
+    if (!text) {
+      return text;
+    }
+
+    const mentionRegex = /@([\p{L}\d._]+)/gu;
+
+    const replacedText = await Promise.all(
+      text.split(mentionRegex).map(async (part, index) => {
+        if (index % 2 === 1) {
+          const fullName = await this.searchUsername(part);
+          return `@${fullName || part}`;
+        }
+        return part;
+      })
+    );
+
+    return replacedText.join("");
+  }
+
+  async searchUsername(username) {
+    username = username.toLowerCase();
+
+    if (cachedNames.has(username)) {
+      return cachedNames.get(username);
+    }
+
+    if (pendingSearch?.usernames.size <= 50) {
+      pendingSearch.usernames.add(username);
+      const results = await pendingSearch.search;
+
+      if (results.searchedUsernames.includes(username)) {
+        const fullName =
+          results.data.users?.find(
+            (user) => user.username.toLowerCase() === username
+          )?.name ||
+          results.data.groups?.find(
+            (group) => group.name.toLowerCase() === username
+          )?.full_name;
+
+        cachedNames.set(username, fullName);
+        return fullName;
+      }
+    }
+
+    return this.deferSearch(username);
+  }
+
+  async deferSearch(username) {
+    const usernamesList = new Set([username]);
+
+    pendingSearch = {
+      search: new Promise((resolve) => {
+        setTimeout(async () => {
+          const searchedUsernames = Array.from(usernamesList);
+          pendingSearch = null;
+
+          const data = await ajax("/u/search/users.json", {
+            data: {
+              usernames: searchedUsernames.join(","),
+              include_groups: this.siteSettings.show_fullname_for_groups,
+            },
+          });
+
+          resolve({ searchedUsernames, data });
+        }, 20);
+      }),
+      usernames: usernamesList,
+    };
+
+    return this.searchUsername(username);
+  }
 
   get postUrl() {
     return getURL(this.args.reaction.post.url);
@@ -16,10 +114,6 @@ export default class DiscourseReactionsReactionPost extends Component {
 
   get emojiUrl() {
     const reactionValue = this.args.reaction.reaction.reaction_value;
-
-    if (!reactionValue) {
-      return;
-    }
-    return emojiUrlFor(reactionValue);
+    return reactionValue ? emojiUrlFor(reactionValue) : null;
   }
 }
--- spec/system/reactions_activity_spec.rb
+++ spec/system/reactions_activity_spec.rb
@@ -5,12 +5,12 @@ describe "Reactions | Activity", type: :system, js: true do
 
   before do
     SiteSetting.discourse_reactions_enabled = true
+
     sign_in(current_user)
   end
 
   context "when current user reacts to a post" do
     fab!(:post_1) { Fabricate(:post) }
-
     before do
       DiscourseReactions::ReactionManager.new(
         reaction_value: "clap",
@@ -23,6 +23,60 @@ describe "Reactions | Activity", type: :system, js: true do
       visit("/u/#{current_user.username}/activity/reactions")
 
       expect(page).to have_css(".user-stream-item [data-post-id='#{post_1.id}']")
+    end
+
+    context "when unicode usernames is enabled " do
+      before do
+        SiteSetting.external_system_avatars_enabled = true
+        SiteSetting.unicode_usernames = true
+      end
+
+      context "when prioritize_full_name_in_ux SiteSetting is true" do
+        before do
+          SiteSetting.prioritize_full_name_in_ux = true
+          stub_request(
+            :get,
+            "https://avatars.discourse-cdn.com/v4/letter/b/dbc845/48.png",
+          ).to_return(status: 200, body: "image", headers: {})
+
+          stub_request(
+            :get,
+            "https://avatars.discourse-cdn.com/v4/letter/b/90ced4/48.png",
+          ).to_return(status: 200, body: "image", headers: {})
+
+          stub_request(
+            :get,
+            "https://avatars.discourse-cdn.com/v4/letter/b/90ced4/24.png",
+          ).to_return(status: 200, body: "image", headers: {})
+
+          stub_request(
+            :get,
+            "https://avatars.discourse-cdn.com/v4/letter/b/90ced4/144.png",
+          ).to_return(status: 200, body: "image", headers: {})
+
+          stub_request(
+            :get,
+            "https://avatars.discourse-cdn.com/v4/letter/b/9de053/48.png",
+          ).to_return(status: 200, body: "image", headers: {})
+        end
+
+        it "shows the name of the mentioned user instead of the username" do
+          unicode_user = Fabricate(:user)
+          post_2 =
+            Fabricate(:post, raw: "This is a test post with a mention @#{unicode_user.username}")
+          DiscourseReactions::ReactionManager.new(
+            reaction_value: "clap",
+            user: current_user,
+            post: post_2,
+          ).toggle!
+
+          visit("/u/#{current_user.username}/activity/reactions")
+
+          post = find(".user-stream-item [data-post-id='#{post_2.id}']")
+          expect(page).to have_css(".user-stream-item [data-post-id='#{post_2.id}']")
+          expect(post).to have_content("@Bruce Wayne")
+        end
+      end
     end
 
     context "when the associated post is deleted" do
