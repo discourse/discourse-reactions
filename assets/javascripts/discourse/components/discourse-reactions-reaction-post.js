83cc86dc5d462000e9a43480041bc099ccfdc331
Juan David Martinez <juan@discourse.org>
2025-03-14 12:07:27 -0500

    UX: Reactions user activity page shows names on mentions when  site setting is true

--- assets/javascripts/discourse/components/discourse-reactions-reaction-post.js
+++ assets/javascripts/discourse/components/discourse-reactions-reaction-post.js
@@ -6,9 +6,36 @@ import { emojiUrlFor } from "discourse/lib/text";
 
 export default class DiscourseReactionsReactionPost extends Component {
   @service site;
+  @service siteSettings;
 
   @equal("args.reaction.post.post_type", "site.post_types.moderator_action")
   moderatorAction;
+
+  get expandedExcerpt() {
+    if (!this.args.reaction.post.expandedExcerpt) {
+      return;
+    }
+
+    return this.displayUsernameOrNameMentionExcerpt(
+      this.args.reaction.post.expandedExcerpt
+    );
+  }
+
+  get excerpt() {
+    return this.displayUsernameOrNameMentionExcerpt(
+      this.args.reaction.post.excerpt
+    );
+  }
+
+  displayUsernameOrNameMentionExcerpt(excerpt) {
+    const name = this.args.reaction.user.name;
+    const username = this.args.reaction.user.username;
+    if (this.siteSettings.prioritize_full_name_in_ux || !username) {
+      return excerpt.replace(/@\w+/, `@${name || username}`);
+    } else {
+      excerpt;
+    }
+  }
 
   get postUrl() {
     return getURL(this.args.reaction.post.url);
