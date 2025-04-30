44fd2fd0a900529e606d8269f90649118d7041fd
Sérgio Saquetim <saquetim@discourse.org>
2025-04-30 00:15:12 -0300

    Changed test scenarios to also use the Glimmer Post Stream and added .discourse-compatibility

--- spec/system/reactions_post_list_spec.rb
+++ spec/system/reactions_post_list_spec.rb
@@ -25,51 +25,57 @@ describe "Reactions | Post reaction user list", type: :system, js: true do
     ).toggle!
   end
 
-  it "shows a list of users who have reacted to a post on hover for likes and each reaction" do
-    sign_in(current_user)
-    visit(post.url)
+  %w[enabled disabled].each do |value|
+    before { SiteSetting.glimmer_post_stream_mode = value }
 
-    page.execute_script("window.isSystemTest = true;")
+    context "when glimmer_post_stream_mode=#{value}" do
+      it "shows a list of users who have reacted to a post on hover for likes and each reaction" do
+        sign_in(current_user)
+        visit(post.url)
 
-    expect(reactions_list).to have_reaction("heart")
-    expect(reactions_list).to have_reaction("clap")
+        page.execute_script("window.isSystemTest = true;")
 
-    reactions_list.hover_over_reaction("heart")
-    expect(reactions_list).to have_users_for_reaction("heart", [user_2.username])
+        expect(reactions_list).to have_reaction("heart")
+        expect(reactions_list).to have_reaction("clap")
 
-    # hover on something else to clear the current hover
-    page.find("#site-logo").hover
+        reactions_list.hover_over_reaction("heart")
+        expect(reactions_list).to have_users_for_reaction("heart", [user_2.username])
 
-    reactions_list.hover_over_reaction("clap")
-    expect(reactions_list).to have_users_for_reaction("clap", [user_3.username])
-  end
+        # hover on something else to clear the current hover
+        page.find("#site-logo").hover
 
-  context "when the site allows anonymous users to like posts" do
-    before do
-      SiteSetting.allow_anonymous_mode = true
-      SiteSetting.allow_likes_in_anonymous_mode = true
-    end
+        reactions_list.hover_over_reaction("clap")
+        expect(reactions_list).to have_users_for_reaction("clap", [user_3.username])
+      end
 
-    it "shows a list of users who have liked a post on hover for unauthenticated users" do
-      visit(post.url)
-      page.execute_script("window.isSystemTest = true;")
+      context "when the site allows anonymous users to like posts" do
+        before do
+          SiteSetting.allow_anonymous_mode = true
+          SiteSetting.allow_likes_in_anonymous_mode = true
+        end
 
-      expect(reactions_list).to have_reaction("heart")
+        it "shows a list of users who have liked a post on hover for unauthenticated users" do
+          visit(post.url)
+          page.execute_script("window.isSystemTest = true;")
 
-      reactions_list.hover_over_reaction("heart")
-      expect(reactions_list).to have_users_for_reaction("heart", [user_2.username])
-    end
+          expect(reactions_list).to have_reaction("heart")
 
-    it "shows a list of users who have liked a post on hover for authenticated users posting anonymously" do
-      anonymous_user = Fabricate(:anonymous)
-      sign_in(anonymous_user)
-      visit(post.url)
-      page.execute_script("window.isSystemTest = true;")
+          reactions_list.hover_over_reaction("heart")
+          expect(reactions_list).to have_users_for_reaction("heart", [user_2.username])
+        end
 
-      expect(reactions_list).to have_reaction("heart")
+        it "shows a list of users who have liked a post on hover for authenticated users posting anonymously" do
+          anonymous_user = Fabricate(:anonymous)
+          sign_in(anonymous_user)
+          visit(post.url)
+          page.execute_script("window.isSystemTest = true;")
 
-      reactions_list.hover_over_reaction("heart")
-      expect(reactions_list).to have_users_for_reaction("heart", [user_2.username])
+          expect(reactions_list).to have_reaction("heart")
+
+          reactions_list.hover_over_reaction("heart")
+          expect(reactions_list).to have_users_for_reaction("heart", [user_2.username])
+        end
+      end
     end
   end
 end
